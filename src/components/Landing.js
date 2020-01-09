746e3ffbef98b44aedc744565e1ed610581e4832
Bivek Gurung <bivek.tamu@gmail.com>
2020-01-09 18:45:29 +1100

    Landing component done

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import logo from '../img/logo.png';
+import globe from '../img/globe.png';
 
-function Landing(props) {
-  return <div>Landing</div>;
+import { NavLink } from 'react-router-dom';
+
+function Landing() {
+  return (
+    <section id='landing-page'>
+      <div className='wrapper'>
+        <h1 className='uppercase'>lorem ipsum</h1>
+        <img src={logo} alt='logo-img' id='logo' />
+        <NavLink to='/home'>
+          <i class='fas fa-hand-point-right'></i>&nbsp;&nbsp;Click begin to get
+          started
+        </NavLink>
+      </div>
+
+      <img src={globe} alt='globe-img' id='globe' />
+    </section>
+  );
 }
-
-Landing.propTypes = {};
 
 export default Landing;
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function NavBar() {
-  return <h1>NavBar</h1>;
-}
+import { NavLink } from 'react-router-dom';
+
+const NavBar = props => {
+  return (
+    <nav id='nav-bar'>
+      <ul>
+        <li>
+          <NavLink to='/home' activeClassName='active'>
+            <i class='fas fa-home'></i> Home
+          </NavLink>
+          <NavLink to='/topic' activeClassName='active'>
+            TOPIC 1
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+NavBar.propTypes = {};
 
 export default NavBar;
